95b87181f311f9c5f3c9fec92543ad412ec92acc
Noam Shurki <shurkin@post.bgu.ac.il>
2024-12-08 17:45:05 +0200

    add welcome component

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../Styles/Welcome.css';
+import axios from 'axios';
 
 function Welcome() {
     const navigate = useNavigate();
@@ -9,16 +9,31 @@ function Welcome() {
         navigate(path);
     };
 
-  return (
+    const handleLogin = async () => {
+        try {
+            console.log('Login process started...');
+            const response = await axios.post('http://localhost:5000/api/login', {
+                username: 'admin', // Hardcoded username
+                password: 'admin-password', // Hardcoded password
+            });
+            console.log('Login successful:', response.data);
+            //navigateTo('/login');
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+
+
+    return (
     <div className="welcome-container">
-      <h1>Welcome to Group Purchase Platform</h1>
-      <div className="button-group">
-        <button onClick={() => navigateTo('/login')}>Login</button>
+        <h1>Welcome to Group Purchase Platform</h1>
+        <div className="button-group">
+        <button onClick={handleLogin}>Login</button>
         <button onClick={() => navigateTo('/register/user')}>Register as User</button>
         <button onClick={() => navigateTo('/register/business')}>Register as Business</button>
-      </div>
+        </div>
     </div>
-  );
+    );
 }
 
 export default Welcome;
