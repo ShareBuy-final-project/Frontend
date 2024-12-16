ead4f82287ecdde893c5b0f5b465b169e00724a4
idonag <idonag@post.bgu.ac.il>
2024-12-16 15:06:06 +0200

    create joingroup modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import coffeeBeansImage from '../assets/images/coffee-beans-white-wooden-wall-closeup.jpg';
 
 const Card = ({ children, bg = 'bg-gray-200', width = 'w-auto', height = 'h-auto', backgroundImage }) => {
 
-  console.log(`HHDHDHDHDHDHH ${backgroundImage}`)
-  console.log(`FFFFFFFFFFFFF ${coffeeBeansImage}`)
   const style = { 
     backgroundImage: ` linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.5))
     ,url(${backgroundImage})`, 
