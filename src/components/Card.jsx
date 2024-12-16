533ec7d86ec13a7dda5b339321efe6c9798e558f
idonag <idonag@post.bgu.ac.il>
2024-12-16 12:04:01 +0200

    background pictures in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,23 @@
-const Card = ({ children, bg = 'bg-gray-200', width = 'w-auto', height = 'h-auto' }) => {
+import React from 'react';
+import coffeeBeansImage from '../assets/images/coffee-beans-white-wooden-wall-closeup.jpg';
+
+const Card = ({ children, bg = 'bg-gray-200', width = 'w-auto', height = 'h-auto', backgroundImage }) => {
+
+  console.log(`HHDHDHDHDHDHH ${backgroundImage}`)
+  console.log(`FFFFFFFFFFFFF ${coffeeBeansImage}`)
+  const style = { 
+    backgroundImage: ` linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.5))
+    ,url(${backgroundImage})`, 
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center',
+    backgroundRepeat:"no-repeat" 
+  }
+
   return (
-    <div className={`${bg} ${width} ${height} p-6 rounded-lg shadow-md`}>
+    <div className={`${bg} ${width} ${height} p-6 rounded-lg shadow-md  flex flex-col justify-between`} style={style}>
       {children}
     </div>
   );
 };
 
-export default Card
+export default Card;
