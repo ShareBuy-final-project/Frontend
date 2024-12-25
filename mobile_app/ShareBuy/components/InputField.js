2d03b36f5f38b7729fc9d18bf1ebf98838d225b7
Noam Shurki <shurkin@post.bgu.ac.il>
2024-12-25 17:53:49 +0200

    add label to inputField

--- mobile_app/ShareBuy/components/InputField.js
+++ mobile_app/ShareBuy/components/InputField.js
@@ -1,33 +1,44 @@
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { COLORS, FONT } from '../constants/theme';
 
-const InputField = ({ icon, placeholder, keyboardType, value, onChangeText, isValid }) => (
-  <View style={[styles.inputContainer, !isValid && styles.invalidInput]}>
-    <Icon name={icon} size={20} color={COLORS.gray} style={styles.icon} />
-    <TextInput
-      style={styles.input}
-      placeholder={placeholder}
-      placeholderTextColor={COLORS.gray}
-      keyboardType={keyboardType}
-      value={value}
-      onChangeText={onChangeText}
-    />
+const InputField = ({ icon, placeholder, keyboardType, value, onChangeText, isValid, label }) => (
+  <View style={styles.container}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={[styles.inputContainer, !isValid && styles.invalidInput]}>
+      <Icon name={icon} size={20} color={COLORS.gray} style={styles.icon} />
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        placeholderTextColor={COLORS.gray}
+        keyboardType={keyboardType}
+        value={value}
+        onChangeText={onChangeText}
+      />
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
+  container: {
+    width: '80%',
+    marginTop: 20,
+  },
+  label: {
+    fontSize: 14,
+    color: COLORS.black,
+    fontFamily: FONT.arial,
+    marginBottom: 5,
+  },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    width: '80%',
     height: 50,
-    borderColor: COLORS.gray2,
+    borderColor: COLORS.black,
     borderWidth: 1,
     borderRadius: 0,
     paddingHorizontal: 10,
-    marginTop: 20,
   },
   invalidInput: {
     borderColor: 'red',
