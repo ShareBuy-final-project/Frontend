b0ee1b5066c2797af7977f6fb01ac13b9b13f306
amit-levints-bgu <levints@post.bgu.ac.il>
2025-01-03 13:01:16 +0200

    BaseLayout was added

--- mobile_app/ShareBuy/app/home.js
+++ mobile_app/ShareBuy/app/home.js
@@ -1,12 +1,21 @@
-import { getToken } from "../utils/userTokens";
-import { Text, View } from "react-native";
-const home = () => {
-    const user = getToken('email');
-    return(
-        <View>
-            <Text>Home</Text>
-            <Text>Hello {user}</Text>
-        </View>
-    )
-}
-export default home;
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import BaseLayout from './BaseLayout'; // Adjust the path based on your folder structure
+
+const Home = () => {
+  return (
+    <BaseLayout>
+      <Text style={styles.contentText}>Welcome to the Home Page!</Text>
+    </BaseLayout>
+  );
+};
+
+const styles = StyleSheet.create({
+  contentText: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
+
+export default Home;
