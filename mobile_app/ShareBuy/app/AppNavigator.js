9830894a9ed63f5eb10fcf4d61b5a3dfe6b39b14
Noam Shurki <shurkin@post.bgu.ac.il>
2025-01-05 21:28:18 +0200

    fix navigation

--- mobile_app/ShareBuy/app/AppNavigator.js
+++ mobile_app/ShareBuy/app/AppNavigator.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from './welcome';
 import Register from './register';
 import RegisterTypeSelection from './registerTypeSelection';
@@ -9,24 +8,24 @@ import RegisterLocation from './registerLocation';
 import RegisterBusinessContactInfo from './registerBusinessContactInfo';
 import RegisterPassword from './registerPassword';
 import home from './home';
+import PersonalInformation from './UserSideBarPages/personalInformation';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="welcome">
-        <Stack.Screen name="welcome" component={Welcome} />
-        <Stack.Screen name="home" component={home} />
-        <Stack.Screen name="register" component={Register} />
-        <Stack.Screen name="registerPassword" component={RegisterPassword} />
-        <Stack.Screen name="registerTypeSelection" component={RegisterTypeSelection} />
-        <Stack.Screen name="registerBusinessDetails" component={RegisterBusinessDetails} />
-        <Stack.Screen name="registerLocation" component={RegisterLocation} />
-        <Stack.Screen name="registerBusinessContactInfo" component={RegisterBusinessContactInfo} />
-        {/* Add other screens here */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="welcome" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="welcome" component={Welcome} />
+      <Stack.Screen name="home" component={home} />
+      <Stack.Screen name="register" component={Register} />
+      <Stack.Screen name="registerPassword" component={RegisterPassword} />
+      <Stack.Screen name="registerTypeSelection" component={RegisterTypeSelection} />
+      <Stack.Screen name="registerBusinessDetails" component={RegisterBusinessDetails} />
+      <Stack.Screen name="registerLocation" component={RegisterLocation} />
+      <Stack.Screen name="registerBusinessContactInfo" component={RegisterBusinessContactInfo} />
+      <Stack.Screen name="personalInformation" component={PersonalInformation} />
+      {/* Add other screens here */}
+    </Stack.Navigator>
   );
 };
 
