60a373321d3820650d706c8c09c9d912fc9683c0
Noam Shurki <shurkin@post.bgu.ac.il>
2025-05-23 18:21:18 +0300

    create business flow

--- run-tests.bat
+++ run-tests.bat
@@ -1,15 +1,7 @@
 @echo off
 setlocal
 
-maestro test .maestro/creatAccount.yml
-maestro test .maestro/login.yml
-maestro test .maestro/searchByName.yml
-maestro test .maestro/clearSearchBar.yml
-maestro test .maestro/searchByCategory.yml
-maestro test .maestro/clearSearchBar.yml
-maestro test .maestro/saveGroup.yml
-maestro test .maestro/joinGroup.yml
-maestro test .maestro/chat.yml
-maestro test .maestro/leaveGroup.yml
+maestro test .maestro/fail_login_flow.yml
+maestro test .maestro/successful_login_logout_flow.yml
 
 endlocal
